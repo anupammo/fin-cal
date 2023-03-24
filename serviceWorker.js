66dd45dc290817bfe152e569fad0ccb665e0c25d
bhritick <hritickbaidya1@gmail.com>
2023-03-24 20:22:19 +0530

    240320232022pm

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,27 @@
 const assets = [
-    "./",
-    "./index.html",
-    "./app.js",
-  ]
-  
-  self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+  "./",
+  "./index.html",
+  "./app.js",
+  "./res/icon/bank-deposit.webp",
+  "./res/icon/deposits.webp",
+  "./res/icon/ssy.webp",
+  "./res/icon/pension-plan.webp",
+  "./res/icon/bima-yojana.webp",
+  "./res/icon/govt-bonds.webp",
+  "./res/icon/gold-bonds.webp",
+  "./marketing-consulting-rafiki.svg",
+  "./marketing-consulting-rafiki.svg",
+  "./marketing-consulting-rafiki.svg",
+  "./marketing-consulting-rafiki.svg",
+  "./marketing-consulting-rafiki.svg",
+  "./marketing-consulting-rafiki.svg",
+  "./marketing-consulting-rafiki.svg",
+]
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
