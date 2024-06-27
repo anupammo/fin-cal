f10bbeb30fc4671ea96191b94455ce0ad3e7dd51
Anupam Mondal <a09051985@gmail.com>
2024-06-28 04:23:48 +0530

    280620240423

--- fincla-nextjs/app/layout.js
+++ fincla-nextjs/app/layout.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
+      <body className={inter.className}>
+        {children}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
+      </body>
     </html>
   );
 }
