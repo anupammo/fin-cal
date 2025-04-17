936c2518dc98b7d2ebc82d190aec2233dfe618a2
Anupam Mondal <a09051985@gmail.com>
2025-04-17 21:13:06 +0530

    Head structured with Canonical, OG and Twitter Card

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,38 @@
+import Head from "next/head";
+
+export const metadata = {
+    title: "About Page",
+    description: "This is the about us page.",
+    alternates: {
+      canonical: "https://yourwebsite.com/about",
+    },
+    openGraph: {
+      title: "About Us",
+      description: "Learn more about us on this page.",
+      url: "https://yourwebsite.com/about",
+      siteName: "Your Website Name",
+      images: [
+        {
+          url: "https://yourwebsite.com/og-image.jpg",
+          width: 1200,
+          height: 600,
+          alt: "About Us Image",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+  };
+  
 export default function About() {
     return (
-        <div>
+        <>
+            <Head>
+                <title>{metadata.title}</title>
+                <meta name="description" content={metadata.description} />
+                <link rel="canonical" href={metadata.canonical} />
+            </Head>
             <h1 className="lead text-center fs-1 py-5">Welcome to <span className="text-danger">About </span>page</h1>
-        </div>
+        </>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,42 @@
+import Head from "next/head";
 // import Image from "next/image";
 import styles from "./page.module.css";
 
+export const metadata = {
+  title: "Nivesguru: Free Calculator Investment make easy for India",
+  description: "Welcome to our website. Explore our products, services, and more.",
+  alternates: {
+    canonical: "https://anupammondal.in/",
+  },
+  openGraph: {
+    title: "Nivesguru: Free Calculator Investment make easy for India",
+    description: "Discover the best products and services at Your Website Name.",
+    url: "https://anupammondal.in/",
+    siteName: "Nivesguru: Free Calculator Investment make easy for India",
+    images: [
+      {
+        url: "https://anupammondal.in/images/home-og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Home Page ImageNivesguru: Free Calculator Investment make easy for India",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1 className="lead text-center fs-1 py-5">Welcome to <span className="text-danger">Homepage</span></h1>
-    </div>
+    <>
+      <Head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+          <link rel="canonical" href={metadata.canonical} />
+      </Head>
+      <div className={styles.page}>
+        <h1 className="lead text-center fs-1 py-5">Welcome to <span className="text-danger">Homepage</span></h1>
+      </div>
+    </>
   );
 }
